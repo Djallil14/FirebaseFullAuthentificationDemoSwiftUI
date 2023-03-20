0ab291092838e6ee6844e0ff6770c3e756029302
Djallil Elkebir <Elkebir.djallil@icloud.com>
2023-03-20 00:34:18 -0400

    Initial Commit

---
+++ FirebaseLoginAndMemberHandling/FirebaseLoginAndMemberHandlingApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseLoginAndMemberHandlingApp.swift
+//  FirebaseLoginAndMemberHandling
+//
+//  Created by Djallil Elkebir on 2023-03-20.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseLoginAndMemberHandlingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
